271cabc5975fedf673cc772d9ab5f23e158c0945
sw1ca <r.swiecicki04@gmail.com>
2025-05-26 17:50:14 +0200

    fix: change function responsible for generating map from walls in random places to random mazes

--- src/model/GameTableModel.java
+++ src/model/GameTableModel.java
@@ -1,36 +1,61 @@
 package model;
 
 import javax.swing.table.AbstractTableModel;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class GameTableModel extends AbstractTableModel {
-    private final int size;
-    private final char[][] board;
+    private GameCell[][] board;
+    private int size;
 
-    public GameTableModel(int size) {
+    public void generateMaze(int size) {
         this.size = size;
-        board = new char[size][size];
-        generateBoard();
-    }
-    private void generateBoard() {
-        for (int y = 0; y < size; y++) {
-            for (int x = 0; x < size; x++) {
-                board[y][x] = '.'; // Empty cell
+        board = new GameCell[size][size];
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                board[i][j] = new GameCell();
+                board[i][j].setWall(true);
             }
         }
+
+        generateMazeDFS(1, 1, new boolean[size][size]);
+
+        // Add dot
         for (int i = 0; i < size; i++) {
-            board[0][i] = '#'; // Top wall
-            board[size - 1][i] = '#'; // Bottom wall
-            board[i][0] = '#'; // Left wall
-            board[i][size - 1] = '#'; // Right wall
-        }
-        for (int y = 2; y < size - 1; y++) {
-            for (int x = 0; x < size - 1; x++) {
-                if(Math.random() < 0.15) {
-                    board[y][x] = '#'; // Random walls
+            for (int j = 0; j < size; j++) {
+                if (!board[i][j].isWall()) {
+                    board[i][j].setDot(true);
                 }
             }
         }
-        board[1][1] = 'P'; // Player start position
+
+        // Player placement
+        board[1][1].setPlayer(true);
+        fireTableStructureChanged();
+    }
+
+    private void generateMazeDFS(int x, int y, boolean[][] visited) {
+        visited[x][y] = true;
+        board[x][y].setWall(false);
+
+        List<int[]> directions = Arrays.asList(new int[]{0, 2}, new int[]{0, -2}, new int[]{2, 0}, new int[]{-2, 0});
+        Collections.shuffle(directions);
+
+        for (int[] direction : directions) {
+            int newX = x + direction[0];
+            int newY = y + direction[1];
+
+            if (isValid(newX, newY) && !visited[newX][newY]) {
+                board[x + direction[0] / 2][y + direction[1] / 2].setWall(false);
+                generateMazeDFS(newX, newY, visited);
+            }
+        }
+    }
+
+    private boolean isValid(int x, int y) {
+        return x > 0 && y > 0 && x < size - 1 && y < size - 1;
     }
 
     @Override
@@ -44,7 +69,15 @@ public class GameTableModel extends AbstractTableModel {
     }
 
     @Override
-    public Object getValueAt(int rowIndex, int columnIndex) {
-        return board[rowIndex][columnIndex];
+    public Object getValueAt(int row, int col) {
+        return board[row][col];
+    }
+
+    public GameCell[][] getBoard() {
+        return board;
+    }
+
+    public GameCell getCell(int row, int col) {
+        return board[row][col];
     }
 }
--- src/view/GameWindow.java
+++ src/view/GameWindow.java
@@ -7,17 +7,23 @@ import javax.swing.table.TableColumn;
 import java.awt.*;
 
 public class GameWindow extends JFrame {
-    private int boardSize;
     private JTable table;
     private GameTableModel tableModel;
     public GameWindow(int size) {
-        this.boardSize = size;
         setTitle("Chase & Chew - Board Size: " + size + "x" + size);
         setSize(800, 600);
         setLocationRelativeTo(null);
 
-        tableModel = new GameTableModel(size);
+        tableModel = new GameTableModel();
+        tableModel.generateMaze(size);
+
         table = new JTable(tableModel);
+        table.setDefaultRenderer(Object.class, new GameCellRenderer());
+        table.setShowGrid(false);
+        table.setIntercellSpacing(new Dimension(0, 0));
+        table.setTableHeader(null);
+        table.setRowSelectionAllowed(false);
+        table.setCellSelectionEnabled(false);
 
         int cellSize = Math.max(5, 700 / size);
         table.setRowHeight(cellSize);
@@ -28,6 +34,13 @@ public class GameWindow extends JFrame {
         JScrollPane scrollPane = new JScrollPane(table);
         table.setTableHeader(null);
         add(scrollPane, BorderLayout.CENTER);
+    }
+
+    public JTable getTable() {
+        return table;
+    }
+    public GameTableModel getTableModel() {
+        return tableModel;
     }
 }
 
